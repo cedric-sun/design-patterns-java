3c29a0174c2d035051fad5e2b1ddfe40f04501e1
Cedric Sun <sunce@ufl.edu>
2019-07-04 16:59:44 +0800

    add chain of resposibility

---
+++ src/cor/Trouble.java
@@ -0,0 +1,18 @@
+package cor;
+
+public class Trouble {
+    private int number;
+
+    public Trouble(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[Trouble #%d]", number);
+    }
+}
